70a04c18d1e973f7e7c15e8112aafada7bc97470
D.B <165270880+BCL-FOSS@users.noreply.github.com>
2025-09-06 19:03:10 +0400

    probe initialization test

--- bcl_umj_probe/init_app.py
+++ bcl_umj_probe/init_app.py
@@ -4,10 +4,7 @@ from fastapi.security import APIKeyHeader
 import redis
 from utils.network_utils.ProbeInfo import ProbeInfo
 import logging
-import uuid
 from passlib.hash import bcrypt
-import sys
-import asyncio
 from utils.RedisDB import RedisDB
 from utils.network_utils.ProbeInfo import ProbeInfo
 from utils.network_utils.NetworkDiscovery import NetworkDiscovery
@@ -19,7 +16,6 @@ logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('passlib').setLevel(logging.ERROR)
 logger = logging.getLogger(__name__)
 
-# Define the API key header scheme (e.g., header name "x-api-key")
 api_key_header = APIKeyHeader(name="x-api-key", auto_error=True)
 prb_db = RedisDB(hostname='localhost', port='6379')
 probe_utils = ProbeInfo()
@@ -28,29 +24,32 @@ net_test = NetworkTest()
 net_utils = NetUtil(interface='')
 net_snmp = NetworkSNMP()
 
-async def probe_init():
-    await prb_db.connect_db()
-    ping = await prb_db.ping_db()
-    logger.info(f'redis db ping result: {ping}')
-    prb_id, hstnm = probe_utils.gen_probe_register_data()
-
-    if await prb_db.get_all_data(match=f'*{hstnm}*', cnfrm=True) is False:
-        probe_data=probe_utils.collect_local_stats(id=f"{prb_id}", hostname=hstnm)
-        probe_data['api_key'] = bcrypt.hash(str(uuid.uuid4()))
-        logger.info(f"API Key for umjiniti probe {id}: {probe_data['api_key']}. Store this is a secure location as it will not be displayed again.")
-        logger.info(probe_data)
-        logger.info(probe_utils.get_ifaces())
-    
-        if await prb_db.upload_db_data(id=f"{prb_id}", data=probe_data) is not None:
-            logger.info(f'probe data for {prb_id} generated successfully')
-        else:
-            logger.error('Probe data generation failed')
-    else:
-        pass
-        
-asyncio.run(probe_init())
-
 # Dependency function to validate the API key
+def validate_api_key(key: str = Depends(api_key_header)):
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)  # decode for str output
+    pong = r.ping()
+    logger.info(f"Redis ping: {pong}")
+
+    _, hostname = probe_utils.gen_probe_register_data()
+    cursor, keys = r.scan(cursor=0, match=f'*{hostname}*')
+
+    if keys:
+        for redis_key in keys:
+            hash_data = r.hgetall(redis_key)
+            logger.info(hash_data)
+            stored_api_key = hash_data.get("api_key")
+
+            if not stored_api_key:
+                raise
+
+            if bcrypt.verify(key, stored_api_key):
+                return key
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Invalid or missing API key"
+                )
+"""
 def validate_api_key(key: str = Depends(api_key_header)):
     r = redis.Redis(host='localhost', port=6379)
     pong = r.ping()
@@ -74,5 +73,7 @@ def validate_api_key(key: str = Depends(api_key_header)):
         else:
             return key
 
-api = FastAPI()
+"""
 
+
+api = FastAPI()
